52016f01d984ca5f0bfba60a28d9883f95f23a26
alumno <benjathje@hotmail.com>
2017-05-19 08:41:04 -0300

    agregamos los define de vuelo

--- clases/vuelos.py
+++ clases/vuelos.py
@@ -13,3 +13,29 @@ class vuelo(object):
     hora = time
     origen = ""
     destino = ""
+
+    def agregarPasajero(self,nom,ape,fec,dni,mil,vi,nes):
+        unPasajero = pasajero
+        unPasajero.agregarPersona(nom,ape,fec,dni)
+        unPasajero.cambiarMVN(mil,vi,nes)
+        self.listaPasajeros.append(unPasajero)
+
+    def agregarPiloto(self,nom,ape,fec,dni,mod):
+        unPiloto = piloto
+        unPiloto.agregarPersona(nom,ape,fec,dni)
+        unPiloto.autorizarModelo(mod)
+        self.listaTripulacion.append(unPiloto)
+
+    def agregarServicioAbordo(self,nom,ape,fec,dni,mod,idi):
+        unServicio = serviciosAbordo
+        unServicio.agregarPersona(nom,ape,fec,dni)
+        unServicio.autorizarModelo(mod)
+        unServicio.agregarIdioma(idi)
+        self.listaTripulacion.append(unServicio)
+
+    def modificarVuelo(self,mod,fec,hor,ori,des):
+        self.modelo = mod
+        self.fecha = fec
+        self.hora = hor
+        self.origen = ori
+        self.destino = des
